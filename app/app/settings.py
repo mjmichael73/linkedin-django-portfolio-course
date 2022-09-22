0e2e11e73d7a6a9b34b2dec617c11b3debb8b5ea
Mojtaba Michael <mojimich2015@gmail.com>
2022-09-22 05:30:41 +0300

    Finalized

--- app/app/settings.py
+++ app/app/settings.py
@@ -72,4 +72,7 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR)
--- app/jobs/urls.py
+++ app/jobs/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('jobs/<int:job_id>', views.detail, name="detail")
 ]

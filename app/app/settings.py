967085628bca56dc57daaa360de7cc0058145cc2
Mojtaba Michael <mojimich2015@gmail.com>
2022-09-22 03:30:17 +0300

    Added jobs app

--- app/app/settings.py
+++ app/app/settings.py
@@ -13,6 +13,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'jobs',
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
